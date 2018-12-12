75e40fcbf60995024f8a08f5ba668298ff785c60
DashaChernobai <dashcherdash@gmail.com>
2018-12-12 15:36:55 +0200

    new commmmit)

--- app/src/main/java/com/example/pupil/myapplication/папка/ResultActivity.java
+++ app/src/main/java/com/example/pupil/myapplication/папка/ResultActivity.java
@@ -5,19 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.pupil.myapplication.MainActivity;
 import com.example.pupil.myapplication.Pupil;
 import com.example.pupil.myapplication.R;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class ResultActivity extends AppCompatActivity {
     Button btnClear;
     Button btnBack;
+    EditText search;
     ImageView btnCloseResult;
     RecyclerView list;
     ArrayList<Pupil> arr;
@@ -32,6 +38,7 @@ public class ResultActivity extends AppCompatActivity {
         btnClear = findViewById(R.id.btnClear);
         btnBack = findViewById(R.id.btnBack);
         btnBack = findViewById(R.id.btnBack);
+        search = findViewById(R.id.search);
         btnCloseResult = findViewById(R.id.btnCloseResult);
         myAdapter = new MyAdapter(arr);
         list = findViewById(R.id.list);
@@ -57,9 +64,33 @@ public class ResultActivity extends AppCompatActivity {
         list.setAdapter(myAdapter);
         list.getAdapter().notifyDataSetChanged();
 
+search.addTextChangedListener(new TextWatcher() {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
     }
 
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+filter(s.toString());
+    }
+});
+    }
+  private void filter(String s) {
+      ArrayList<Pupil> arrTmp = new ArrayList<>();
+      for (Pupil p:arr){
+          if ((p.getName().toLowerCase().contains(s.toLowerCase())) || (p.getSurname().toLowerCase().contains(s.toLowerCase()))){
+              arrTmp.add(p);
+          }
+      }
+      list.setAdapter(new MyAdapter(arrTmp));
+      list.getAdapter().notifyDataSetChanged();
+  }
 
     private void startMainActivity() {
         Intent a = new Intent(this, MainActivity.class);
